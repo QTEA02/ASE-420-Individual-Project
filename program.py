487824803143ac32b9977e54d316baba914ad5f3
Quay Robinson <93289432+QTEA02@users.noreply.github.com>
2023-11-20 09:08:07 -0500

    Update program.py

--- program.py
+++ program.py
@@ -1,81 +1,189 @@
-import datetime
-import sqlite3
+class Query:
+    def __init__(self, time_record_repository):
+        self.time_record_repository = time_record_repository
 
-# Connect to the SQLite database
-conn = sqlite3.connect("time_records.db")
-cursor = conn.cursor()
+    def execute_query(self, query_value):
+        pass
 
-# Drop the table if it exists
-cursor.execute("DROP TABLE IF EXISTS time_records")
-conn.commit()
 
-# Create the table
-cursor.execute('''CREATE TABLE time_records (
-                    id INTEGER PRIMARY KEY,
-                    record_date TEXT,
-                    start_time TEXT,
-                    end_time TEXT,
-                    task TEXT,
-                    tag TEXT
-                )''')
-conn.commit()
+class DateQuery(Query):
+    def execute_query(self, query_value):
+        records = self.time_record_repository.database_handler.fetch_all(
+            "SELECT * FROM time_records WHERE record_date = ?", (query_value,)
+        )
+        self.display_results(records)
 
-def record_time():
-    try:
-        date = input("Enter Date (YYYY/MM/DD): ")
-        start_time = input("Enter Time from (HH:MM AM/PM): ")
-        end_time = input("Enter Time to (HH:MM AM/PM): ")
-        task = input("Enter Task: ")
-        tag = input("Enter Tag: ")
+    def display_results(self, records):
+        if not records:
+            print(f"No records found.")
+        else:
+            print(f"\nQuery Results:")
+            for record in records:
+                print(record)
 
-        # Ensure the date format is correct
-        date_format = "%Y/%m/%d"
-        date = datetime.datetime.strptime(date, date_format).strftime(date_format)
 
-        # Parse and format the start time
-        start_time_parts = start_time.split()
-        if len(start_time_parts) != 2:
-            raise ValueError
-        start_time_value = datetime.datetime.strptime(start_time_parts[0], "%I:%M")
-        if start_time_parts[1].lower() == 'pm':
-            start_time_value = start_time_value.replace(hour=start_time_value.hour + 12)
-        start_time = start_time_value.strftime("%H:%M")
+class TaskQuery(Query):
+    def execute_query(self, query_value):
+        records = self.time_record_repository.database_handler.fetch_all(
+            "SELECT * FROM time_records WHERE task = ?", (query_value,)
+        )
+        self.display_results(records)
 
-        # Parse and format the end time
-        end_time_parts = end_time.split()
-        if len(end_time_parts) != 2:
-            raise ValueError
-        end_time_value = datetime.datetime.strptime(end_time_parts[0], "%I:%M")
-        if end_time_parts[1].lower() == 'pm':
-            end_time_value = end_time_value.replace(hour=end_time_value.hour + 12)
-        end_time = end_time_value.strftime("%H:%M")
+    def display_results(self, records):
+        if not records:
+            print(f"No records found.")
+        else:
+            print(f"\nQuery Results:")
+            for record in records:
+                print(record)
 
-        cursor.execute("INSERT INTO time_records (record_date, start_time, end_time, task, tag) VALUES (?, ?, ?, ?, ?)",
-                       (date, start_time, end_time, task, tag))
-        conn.commit()
-        print("Time recorded successfully!")
-    except ValueError:
-        print("Invalid input format. Please use the specified formats.")
 
-def print_database():
-    cursor.execute("SELECT * FROM time_records")
-    records = cursor.fetchall()
-    print("\nDatabase Contents:")
-    for record in records:
-        print(record)
+class TagQuery(Query):
+    def execute_query(self, query_value):
+        records = self.time_record_repository.database_handler.fetch_all(
+            "SELECT * FROM time_records WHERE tag = ?", (query_value,)
+        )
+        self.display_results(records)
 
-while True:
-    print("\n1. Record Time")
-    print("2. Print Database Contents")
-    print("3. Exit")
-    choice = input("Select an option (1/2/3): ")
+    def display_results(self, records):
+        if not records:
+            print(f"No records found.")
+        else:
+            print(f"\nQuery Results:")
+            for record in records:
+                print(record)
 
-    if choice == "1":
-        record_time()
-    elif choice == "2":
-        print_database()
-    elif choice == "3":
-        conn.close()
-        break
-    else:
-        print("Invalid choice. Please select a valid option.")
+
+class QueryHandler:
+    def __init__(self, time_record_repository):
+        self.time_record_repository = time_record_repository
+
+    def query_records(self, query_type, query_value):
+        query = self.create_query(query_type)
+        query.execute_query(query_value)
+
+    def create_query(self, query_type):
+        if query_type == "date":
+            return DateQuery(self.time_record_repository)
+        elif query_type == "task":
+            return TaskQuery(self.time_record_repository)
+        elif query_type == "tag":
+            return TagQuery(self.time_record_repository)
+        else:
+            raise ValueError("Invalid query type. Please use 'date', 'task', or 'tag'.")
+
+
+class InputHandler:
+    @staticmethod
+    def get_user_input(prompt, input_type=str):
+        while True:
+            try:
+                user_input = input(prompt)
+                return input_type(user_input)
+            except ValueError:
+                print("Invalid input. Please try again.")
+
+
+class TimeRecordApp:
+    def __init__(self, time_record_repository, input_handler, query_handler):
+        self.time_record_repository = time_record_repository
+        self.input_handler = input_handler
+        self.query_handler = query_handler
+
+    def run(self):
+        self.time_record_repository.create_table()
+
+        while True:
+            self.print_menu()
+            choice = self.input_handler.get_user_input("Select an option (1/2/3/4): ", str)
+
+            if choice == "1":
+                self.record_time()
+            elif choice == "2":
+                self.print_database()
+            elif choice == "3":
+                self.query_records_menu()
+            elif choice == "4":
+                self.shutdown()
+                break
+            else:
+                print("Invalid choice. Please select a valid option.")
+
+    def print_menu(self):
+        print("\n1. Record Time")
+        print("2. Print Database Contents")
+        print("3. Query Records")
+        print("4. Exit")
+
+    def record_time(self):
+        try:
+            date = self.input_handler.get_user_input("Enter Date (YYYY/MM/DD): ", str)
+            start_time = self.input_handler.get_user_input("Enter Time from (HH:MM AM/PM): ", str)
+            end_time = self.input_handler.get_user_input("Enter Time to (HH:MM AM/PM): ", str)
+            task = self.input_handler.get_user_input("Enter Task: ", str)
+            tag = self.input_handler.get_user_input("Enter Tag: ", str)
+
+            time_record = TimeRecord(date, start_time, end_time, task, tag)
+            self.time_record_repository.record_time(time_record)
+            print("Time recorded successfully!")
+        except ValueError as e:
+            print(f"Error recording time: {str(e)}")
+
+    def print_database(self):
+        try:
+            self.time_record_repository.print_database()
+        except Exception as e:
+            print(f"Error fetching database records: {str(e)}")
+
+    def query_records_menu(self):
+        print("\nQuery Options:")
+        print("1. Query by Date")
+        print("2. Query by Task")
+        print("3. Query by Tag")
+        print("4. Back to Main Menu")
+
+        query_choice = self.input_handler.get_user_input("Select an option (1/2/3/4): ", str)
+
+        if query_choice == "1":
+            self.query_records("date", self.input_handler.get_user_input("Enter Date (YYYY/MM/DD): ", str))
+        elif query_choice == "2":
+            self.query_records("task", self.input_handler.get_user_input("Enter Task: ", str))
+        elif query_choice == "3":
+            self.query_records("tag", self.input_handler.get_user_input("Enter Tag: ", str))
+        elif query_choice == "4":
+            pass  # Back to the main menu
+        else:
+            print("Invalid choice. Returning to the main menu.")
+
+    def query_records(self, query_type, query_value):
+        self.query_handler.query_records(query_type, query_value)
+
+    def shutdown(self):
+        try:
+            self.time_record_repository.database_handler.close_connection()
+            print("Exiting the application. Goodbye!")
+        except Exception as e:
+            print(f"Error closing the database connection: {str(e)}")
+
+
+# Example usage:
+if __name__ == "__main__":
+    connection_string = "time_records.db"
+    table_creation_query = '''CREATE TABLE IF NOT EXISTS time_records (
+                                id INTEGER PRIMARY KEY,
+                                record_date TEXT,
+                                start_time TEXT,
+                                end_time TEXT,
+                                task TEXT,
+                                tag TEXT
+                            )'''
+
+    database_handler = DatabaseHandler(connection_string)
+    database_handler.create_table(table_creation_query)
+
+    time_record_repository = TimeRecordRepository(database_handler)
+    input_handler = InputHandler()
+    query_handler = QueryHandler(time_record_repository)
+    time_record_app = TimeRecordApp(time_record_repository, input_handler, query_handler)
+
+    time_record_app.run()
