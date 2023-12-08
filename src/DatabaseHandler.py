0a74936f366fd4c01ee01e859a55a386e50751ad
Quay Robinson <93289432+QTEA02@users.noreply.github.com>
2023-12-08 16:43:41 -0500

    Update DatabaseHandler.py

--- src/DatabaseHandler.py
+++ src/DatabaseHandler.py
@@ -4,6 +4,8 @@ class DatabaseHandler:
     def __init__(self, connection_string):
         self.conn = sqlite3.connect(connection_string)
         self.cursor = self.conn.cursor()
+        self.build_query = []
+        self.success = False
         self.create_table('''CREATE TABLE IF NOT EXISTS time_records (
                               id INTEGER PRIMARY KEY,
                               record_date TEXT,
@@ -17,32 +19,31 @@ class DatabaseHandler:
         self.cursor.execute(table_creation_query)
         self.conn.commit()
 
-   
-    # can be removed by using a list, and appending query and parameters if you have them.
-    # ex add query to list. IF parameters, add parameters to list
-    # after that you can use a spread operator with the execute() to input add args.
     def execute_query(self, query, parameters=None):
-        if parameters is not None:
-            self.cursor.execute(query, parameters)
+        try:
+            self.build_query.append(query)
+            self.build_query.append(parameters)
+            self.cursor.execute(*self.build_query)
             self.conn.commit()
-            return True
-        elif parameters is None:
-            self.cursor.execute(query)
-            self.conn.commit()
-            return True
-        else:
-            return False 
+            self.succcess = True
+        except sqlite3.Error as e:
+            print("Error during commit:", e)
+            self.conn.close()
+        finally:
+            return self.succcess
        
+    def fetch_all(self, query, parameters):
        
-    # can be removed by using a list, and appending query and parmeters if you have them.
-    # ex add query to list. IF parameters, add parameters to list
-    # after that you can use a spread operator with the execute() to input add args.
-    def fetch_all(self, query, parameters=None):
-        if parameters:
-            self.cursor.execute(query, parameters)
-        else:
-            self.cursor.execute(query)
-        return self.cursor.fetchall()
+        try:
+            self.build_query.clear()
+            self.build_query.append(query)
+            self.build_query.append(parameters)
+            print(self.build_query)
+            self.cursor.execute(*self.build_query)
+            return self.cursor.fetchall()
+        except sqlite3.Error as e:
+            print("Error during commit:", e)
+            self.conn.close()
 
     def close_connection(self):
         self.conn.close()
